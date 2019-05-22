f3ef78e43f657986c94e9b41c9101b4cd3e31339
Gerald Manipon <pymonger@gmail.com>
2019-05-22 18:06:59 +0000

    add mozart init.sh script

---
+++ mozart/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}/.."; pwd)
+source ${BASE_PATH}/funcs
+
+
+# globals
+comp_dir=$SDS_CLUSTER_DIR/mozart
+
+
+# create log directories
+mkdir -p $comp_dir/var/log/elasticsearch \
+         $comp_dir/var/log/httpd \
+         $comp_dir/var/log/rabbitmq \
+         $comp_dir/var/log/redis
+
+
+# create lib directories
+mkdir -p $comp_dir/var/lib/elasticsearch/data \
+         $comp_dir/var/lib/redis \
+         $comp_dir/var/lib/rabbitmq
+
+
+# copy configs
+cp -rp mozart/config ~/.sds/cluster/mozart/etc
