bd57ab604b5ca8089a8ce5ca6e4fa9c62d26255d
Gerald Manipon <pymonger@gmail.com>
2019-05-24 15:06:28 +0000

    move redis-config to global

--- mozart/init.sh
+++ mozart/init.sh
@@ -53,11 +53,19 @@ mkdir -p $comp_dir/var/lib/elasticsearch/data \
          $comp_dir/var/lib/rabbitmq
 
 
-# copy configs
+# setup etc directory
 if [ -e "$comp_dir/etc" ]; then
   rsync -rptvzL $comp_dir/etc/ $comp_dir/etc.bak > /dev/null
 else
   mkdir -p $comp_dir/etc
 fi
-cp -rp $prompt $BASE_PATH/config/* $comp_dir/etc/ # copy global configs
-cp -rp $prompt mozart/config/* $comp_dir/etc/     # copy component-specific configs
+
+
+# copy global configs
+cp -rp $prompt $BASE_PATH/config/datasets.json $comp_dir/etc/
+cp -rp $prompt $BASE_PATH/config/logging.yml $comp_dir/etc/
+cp -rp $prompt $BASE_PATH/config/redis-config $comp_dir/etc/
+
+
+# copy component-specific configs
+cp -rp $prompt mozart/config/* $comp_dir/etc/
